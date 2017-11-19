dc4d6ccab82615b2dc19cd12c84d9c47759e8512
Dvir570 <33749855+Dvir570@users.noreply.github.com>
2017-11-20 00:44:01 +0200

    KML.java using JAK api

--- src/KML.java
+++ src/KML.java
@@ -1,50 +1,28 @@
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
 
 public class KML {
 
 	public void makeKML(ArrayList<WiFi> toDisplay) {
-		final String header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		String kml = "";
-
-		kml += header + "\n" + OpenTag("kml", "xmlns=\"http://www.opengis.net/kml/2.2\"") + "\n"
-				+ OpenTag("Document") + "\n";
+		Kml kml = new Kml();
+		Document doc = kml.createAndSetDocument();
 		for (int i = 0; i < toDisplay.size(); i++) {
 			WiFi w = toDisplay.get(i);
-			kml += OpenTag("Placemark") + "\n" + OpenTag("name") + w.getSSID() + CloseTag("name") + "\n"
-					+ OpenTag("description") + "mac: " + w.getMac() + OpenTag("br/") + " freq: " + w.getFreq()
-					+ OpenTag("br/") + " signal: " + w.getSignal() + CloseTag("description") + "\n"
-					+ OpenTag("Point") + "\n" + OpenTag("coordinates") + w.getLon() + "," + w.getLat() + ","
-					+ w.getAlt() + CloseTag("coordinates") + "\n" + CloseTag("Point") + "\n"
-					+ CloseTag("Placemark") + "\n";
+			doc.createAndAddPlacemark().withName(w.getSSID()).withOpen(Boolean.TRUE)
+					.withDescription("mac: " + w.getMac() + " freq: " + w.getFreq() + " signal: " + w.getSignal())
+					.createAndSetPoint().addToCoordinates(Double.parseDouble(w.getLon()),
+							Double.parseDouble(w.getLat()), Double.parseDouble(w.getAlt()));
 		}
-		kml += CloseTag("Document") + "\n" + CloseTag("kml");
 		try {
-			FileWriter fw = new FileWriter(
-					"C:\\Users\\dvir\\Documents\\Ariel University\\Eclipse Workspace\\Matala0\\bin\\KML Files\\placemarkes.kml");
-			PrintWriter pw = new PrintWriter(fw);
-			pw.write(kml);
-			pw.close();
-			fw.close();
-
+			kml.marshal(new File(
+					"C:\\Users\\dvir\\Documents\\Ariel University\\Eclipse Workspace\\Matala0\\bin\\KML Files\\placemarkes.kml"));
 		} catch (IOException ex) {
 			System.out.print("Error reading file\n" + ex);
 			System.exit(2);
 		}
-	}
-
-	
-	public String OpenTag(String nameTag) {
-		return "<" + nameTag + ">";
-	}
-
-	public String OpenTag(String nameTag, String property) {
-		return "<" + nameTag + " " + property + ">";
-	}
-
-	public String CloseTag(String nameTag) {
-		return "</" + nameTag + ">";
 	}
 }
