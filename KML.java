8c2383dca73a10cded8d2f99bee44703b458d1c2
Dvir570 <33749855+Dvir570@users.noreply.github.com>
2017-11-21 20:53:40 +0200

    Update KML.java

--- KML.java
+++ KML.java
@@ -4,25 +4,37 @@ import java.util.ArrayList;
 
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.TimePrimitive;
+import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
 
 public class KML {
 
 	public void makeKML(ArrayList<WiFi> toDisplay) {
 		Kml kml = new Kml();
 		Document doc = kml.createAndSetDocument();
+		String time;
 		for (int i = 0; i < toDisplay.size(); i++) {
 			WiFi w = toDisplay.get(i);
-			doc.createAndAddPlacemark().withName(w.getSSID()).withOpen(Boolean.TRUE)
-					.withDescription("mac: " + w.getMac() + " freq: " + w.getFreq() + " signal: " + w.getSignal())//.withTimePrimitive(timePrimitive)
+			time = convertTimeFormat(w.getTime());
+			TimeStamp ts = new TimeStamp();
+			ts.setWhen(time);
+			doc.createAndAddPlacemark().withName(w.getSSID()).withOpen(Boolean.TRUE).withTimePrimitive(ts)
+					.withDescription("mac: " + w.getMac() + " freq: " + w.getFreq() + " signal: " + w.getSignal())
 					.createAndSetPoint().addToCoordinates(Double.parseDouble(w.getLon()),
 							Double.parseDouble(w.getLat()), Double.parseDouble(w.getAlt()));
 		}
 		try {
-			kml.marshal(new File(
-					"C:\\Users\\dvir\\Documents\\Ariel University\\Eclipse Workspace\\Matala0\\src\\KML Files\\placemarkes.kml"));
+			kml.marshal(new File("src\\placemarks.kml"));
 		} catch (IOException ex) {
 			System.out.print("Error reading file\n" + ex);
 			System.exit(2);
 		}
+	}
+
+	//ONEPLUS A3003
+	private String convertTimeFormat(String oldTimeFormat) {
+		String[] dateTime = oldTimeFormat.split(" ");
+		System.out.println(dateTime[0] + 'T' + dateTime[1]);
+		return dateTime[0] + 'T' + dateTime[1];
 	}
 }
