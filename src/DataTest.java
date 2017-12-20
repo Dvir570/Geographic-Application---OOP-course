f7d3d8f8c3fa0101146ce535d3a671c121a1080e
Dvir Barzilay <dvir.barzilay@gmail.com>
2017-12-20 23:05:29 +0200

    jUnit commit

--- src/DataTest.java
+++ src/DataTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.Test;
 
@@ -8,25 +9,50 @@ public class DataTest {
 
 	@Test
 	public void testData() {
-		ArrayList<WiFi> wifis = new ArrayList<WiFi>();
-		wifis.add(new WiFi("", "", "", "","", "", "","",""));
-		wifis.add(new WiFi("", "", "", "","", "", "","0",""));
-		wifis.add(new WiFi("", "", "", "","", "", "","-130",""));
-		wifis.add(new WiFi("", "", "", "","", "", "","-40",""));
+		HashMap<String,WiFi> wifis = new HashMap<String,WiFi>();
+		wifis.put("m1", new WiFi("", "m1", "", "","", "", "","",""));
+		wifis.put("m2",new WiFi("", "m2", "", "","", "", "","0",""));
+		wifis.put("m3",new WiFi("", "m3", "", "","", "", "","-130",""));
+		wifis.put("m4",new WiFi("", "m4", "", "","", "", "","-40",""));
 		
 		ArrayList<WiFi> input = new ArrayList<WiFi>();
-		input.add(new WiFi("", "", "", "","", "", "","-40",""));
-		
+		input.add(new WiFi("", "m4", "", "","", "", "","-40",""));
+		input.add(new WiFi("", "m2", "", "","", "", "","-50",""));
+		input.add(new WiFi("", "m3", "", "","", "", "","-60",""));
+		Data d = new Data(wifis, input);
+		assertTrue(d.getPi()==1.48320439700778);
 	}
 
 	@Test
 	public void testGetPi() {
-		fail("Not yet implemented");
+		HashMap<String,WiFi> wifis = new HashMap<String,WiFi>();
+		wifis.put("m1", new WiFi("", "m1", "", "","", "", "","",""));
+		wifis.put("m2",new WiFi("", "m2", "", "","", "", "","0",""));
+		wifis.put("m3",new WiFi("", "m3", "", "","", "", "","-130",""));
+		wifis.put("m4",new WiFi("", "m4", "", "","", "", "","-40",""));
+		
+		ArrayList<WiFi> input = new ArrayList<WiFi>();
+		input.add(new WiFi("", "m4", "", "","", "", "","-40",""));
+		input.add(new WiFi("", "m2", "", "","", "", "","-50",""));
+		input.add(new WiFi("", "m3", "", "","", "", "","-60",""));
+		Data d = new Data(wifis, input);
+		assertNotNull(d.getPi());
 	}
 
 	@Test
 	public void testGetWifis() {
-		fail("Not yet implemented");
+		HashMap<String,WiFi> wifis = new HashMap<String,WiFi>();
+		wifis.put("m1", new WiFi("", "m1", "", "","", "", "","",""));
+		wifis.put("m2",new WiFi("", "m2", "", "","", "", "","0",""));
+		wifis.put("m3",new WiFi("", "m3", "", "","", "", "","-130",""));
+		wifis.put("m4",new WiFi("", "m4", "", "","", "", "","-40",""));
+		
+		ArrayList<WiFi> input = new ArrayList<WiFi>();
+		input.add(new WiFi("", "m4", "", "","", "", "","-40",""));
+		input.add(new WiFi("", "m2", "", "","", "", "","-50",""));
+		input.add(new WiFi("", "m3", "", "","", "", "","-60",""));
+		Data d = new Data(wifis, input);
+		assertNotNull(d.getWifis());
 	}
 
 }
