c9d3b32e05c9199ad1fba2bcef5debce40b8f133
samshimoni <samshimoni@gmail.com>
2018-01-07 14:05:34 +0200

    Algo1 on server

--- src/AlgorithmI.java
+++ src/AlgorithmI.java
@@ -1,66 +1,33 @@
 package src;
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 public class AlgorithmI {
-	private File  fileOut;
-	private ArrayList<AvgMacPoint> avgMacPoints;
-	private ArrayList<WiFi> DB;
 
-	public AlgorithmI(String pathDB, String pathOut) {
+	private AvgMacPoint avgMacPoint;
+	private HashMap<String, ArrayList<WiFi>> DB;
 
-		this.fileOut = new File(pathOut);
-		IOfiles readDB = new IOfiles(pathDB);
-		this.DB = new ArrayList<WiFi>();
-		String headersFile = readDB.readLine();
-		String[] sRow;
-		String nextRow = readDB.readLine();
-		while (nextRow != null && !nextRow.equals("")) {
-			sRow = nextRow.split(",");
-			int countWifi = Integer.parseInt(sRow[5]);
-			for (int i = 0; i < countWifi; i++) {
-				this.DB.add(new WiFi(sRow[1], sRow[7+(4*(i))], sRow[6+(4*(i))], sRow[8+(4*(i))], sRow[3], sRow[2], sRow[4], sRow[9+(4*(i))], sRow[0]));
-			}
-			nextRow = readDB.readLine();
+	public AlgorithmI(Set<Row> DB, String mac) {
+		ArrayList<Row> temp = new ArrayList<Row>(DB);
+		ArrayList<WiFi> wifis = new ArrayList<WiFi>();
+		for (int i = 0; i < temp.size(); i++) {
+			wifis.addAll(temp.get(i).getWifis());
 		}
-		sortByMac();
-		System.out.println(this.DB.size());
-		readDB.close();
+		ArrayList<WiFi> commonMac = new ArrayList<WiFi>();
+		for (int i = 0; i < wifis.size(); i++) {
+			if (wifis.get(i).getMac().equals(mac))
+				commonMac.add(wifis.get(i));
+		}
+		if (commonMac.isEmpty())
+			this.avgMacPoint = null;
+		else
+			this.avgMacPoint = new AvgMacPoint(commonMac);
 	}
 
-	private void sortByMac() {
-		if (DB.size() == 0)
-			return;
+	public AvgMacPoint getAvgMacPoint() {
+		return avgMacPoint;
+	}
 
-		ArrayList<Row> ar = new ArrayList<Row>();
-		Row r = new Row();
-		ar.add(r);
-		r.add(DB.get(0));		
-		for (int i = 1; i < DB.size(); i++) {
-			for (int j = 0; j < ar.size(); j++) {
-				if (DB.get(i).getMac().equals(ar.get(j).getWiFi(0).getMac())) {
-					ar.get(j).add(DB.get(i));
-					break;
-				} else if (j == (ar.size() - 1)) {
-					r = new Row();
-					r.add(DB.get(i));
-					ar.add(r);
-					break;
-				}
-			}
-		}
-		this.avgMacPoints = new ArrayList<AvgMacPoint>();
-		for (int i = 0; i < ar.size(); i++) {
-			avgMacPoints.add(new AvgMacPoint(ar.get(i).getWifis()));
-		}
-	}
-	
-	public void writeCsv() {
-		IOfiles writeCsvPoints = new IOfiles(this.fileOut.getPath());
-		for (int i=0;i<avgMacPoints.size();i++) {
-			writeCsvPoints.writeLine(i+","+avgMacPoints.get(i).toString());
-		}
-		writeCsvPoints.close();
-	}
 }
